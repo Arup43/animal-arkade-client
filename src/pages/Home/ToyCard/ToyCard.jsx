ce3594083dedbb528d80c92ee6a344e3a87b7717
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-05-22 02:07:42 +0600

    Added toast notification on view details click

--- src/pages/Home/ToyCard/ToyCard.jsx
+++ src/pages/Home/ToyCard/ToyCard.jsx
@@ -1,9 +1,20 @@
 import { Button, Card } from "react-bootstrap";
 import "./ToyCard.css";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
+import { toast } from "react-toastify";
 
 const ToyCard = ({ toy }) => {
   const { pictureUrl, name, price, rating, description } = toy;
+  const { user } = useContext(AuthContext);
+
+  const handleViewDetails = () => {
+    if (!user) {
+      toast.error("You have to log in first to view details");
+    }
+  };
+
   return (
     <Card style={{ width: "100%" }}>
       <Card.Img variant="top" src={pictureUrl} />
@@ -13,7 +24,9 @@ const ToyCard = ({ toy }) => {
         <p>Price: {price}</p>
         <p>Rating: {rating}</p>
         <Link to={`/toy/${toy._id}`}>
-          <Button variant="success">View details</Button>
+          <Button onClick={handleViewDetails} variant="success">
+            View details
+          </Button>
         </Link>
       </Card.Body>
     </Card>
