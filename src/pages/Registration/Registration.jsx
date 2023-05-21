470993ce2d4a9617530dd7c114a357a7b79b9fba
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-05-21 20:32:59 +0600

    Registration page design done

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,11 +1,59 @@
-
+import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Registration = () => {
-    return (
-        <div>
-            <h1>This is registration page</h1>
-        </div>
-    );
+  return (
+    <Container className="w-25 mx-auto mb-5 pb-5 mt-5 pt-5">
+      <h3>Please Register</h3>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Photo URL</Form.Label>
+          <Form.Control
+            type="text"
+            name="photo"
+            placeholder="Photo URL"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Text className="text-success">{}</Form.Text>
+        <Form.Text className="text-danger">{}</Form.Text> <br />
+        <Button variant="success" type="submit">
+          Register
+        </Button>
+        <br />
+        <Form.Text className="text-secondary">
+          Already Have an Account? <Link to="/login">Login</Link>
+        </Form.Text>
+      </Form>
+    </Container>
+  );
 };
 
 export default Registration;
